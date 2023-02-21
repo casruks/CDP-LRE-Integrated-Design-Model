1159fda13bff79706df99e873a3b108d0bf75f2c
casruks <81488457+casruks@users.noreply.github.com>
2023-02-21 23:54:28 +0100

    Update Injector_fn.py

--- Injector_fn.py
+++ Injector_fn.py
@@ -1,5 +1,5 @@
 import numpy as np
-# injector(1712, 212, 1.2, 9.80665, 1137, 870, 1e-3, 1e-3, 5.52e5, 6.9e5, 1)
+# injector(1712, 212, 1.2, 9.80665, 1137, 870, 1.93e-3, 1.77e-3, 5.52e5, 6.9e5, 1)
 
 def injector(F, Isp, OF, g0, rho_ox, rho_f, D_i_ox, D_i_f, dp_ox, dp_f, K_comp):
 
@@ -26,8 +26,10 @@ def injector(F, Isp, OF, g0, rho_ox, rho_f, D_i_ox, D_i_f, dp_ox, dp_f, K_comp):
 
     A_I_ox = 1
     A_I_f = 1
-    v_i = [Velocity(Discharge(K_comp, A_i[0], A_I_ox)[0], dp_ox, rho_ox), \
-           Velocity(Discharge(K_comp, A_i[1], A_I_f)[0], dp_f, rho_f)]
+    #C_d = Discharge(K_comp, A_i[0], A_I_ox)[0]
+    #C_d = Discharge(K_comp, A_i[1], A_I_f)[0], placeholders Q1 are below:
+    v_i = [Velocity(0.87, dp_ox, rho_ox), \
+           Velocity(0.91, dp_f, rho_f)]
     
     n = [m[0]/(rho_ox*v_i[0]*A_i[0]), m[1]/(rho_f*v_i[1]*A_i[1])]
     
@@ -39,3 +41,38 @@ def injector(F, Isp, OF, g0, rho_ox, rho_f, D_i_ox, D_i_f, dp_ox, dp_f, K_comp):
     Q_i = [m_i[0]/ rho_ox, m_i[1] / rho_f]    
 
     return p_inj, v_i, m, m_i, Q_i, n
+
+## Reference values - LRE combustor Q1
+C_dox = 0.87
+C_df = 0.91
+rho_ox = 1137
+rho_f = 817
+OF = 1.2
+p_c = 20.68e5
+p_if = 27.58e5
+p_iox = 26.2e5
+dp_ox = p_iox - p_c
+dp_f = p_if - p_c
+n = 5
+F = 1712
+Isp = 212
+
+v_i_ox = C_dox * (2*dp_ox/rho_ox)**0.5
+v_i_f = C_df * (2*dp_f/rho_f)**0.5
+
+m = F/ (Isp*9.80665)
+m_ox = (OF/(OF+1))*m
+m_f = m - m_ox
+
+m_i_ox = m_ox / n
+m_i_f = m_f / n
+
+Q_i_ox = m_i_ox / rho_ox 
+Q_i_f = m_i_f / rho_f
+
+A_i_ox = Q_i_ox / v_i_ox
+A_i_f = Q_i_f / v_i_f
+
+D_i_ox = (4*A_i_ox/np.pi)**0.5
+D_i_f = (4*A_i_f/np.pi)**0.5
+print(D_i_ox*1000, D_i_f*1000)
