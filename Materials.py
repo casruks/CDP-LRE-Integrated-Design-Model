f0168a39e2dbf11a0dde4c278d11a8b38ae2e746
IMPax478 <batchu.abhi@gmail.com>
2023-03-03 11:48:31 +0100

    Update Materials.py

--- Materials.py
+++ Materials.py
@@ -1,3 +1,6 @@
+import math as mth
+
+
 class Materials:
     def __init__(self, material, density, yieldstress_l, yieldstress_u, Emod, OpTemp_l, OpTemp_u, cost):
         self.material = material
@@ -8,9 +11,38 @@ class Materials:
         self.OpTemp_l = OpTemp_l
         self.OpTemp_u = OpTemp_u
         self.cost = cost
-        self.k=1.0
-
 
 Rhenium = Materials('Rhenium', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938)
 Rhenium2 = Materials('Rhenium2', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938)
 
+def Material_Select(pressure):
+    a = [Rhenium, Rhenium2]
+    for i in a:
+        if i.yieldstress_l > pressure:
+            a = i.material
+            print(a)
+    return a
+
+
+#Computing Mass Conical: 
+x = [(1,2),(3,4),(5,6)]
+ 
+def Mass(x,t,material):
+    total_dist = 0
+    for i in range(len(x)-1):
+        total_dist += mth.dist(x[i+1],x[i])
+    
+    vol = total_dist*t*2*mth.pi
+    Mass = vol*material.density
+    print(Mass)
+    return Mass
+
+
+Mass(x,0.1,Rhenium)
+Material_Select(25)
+
+
+
+
+
+    
