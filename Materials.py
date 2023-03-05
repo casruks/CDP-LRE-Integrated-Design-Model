a54aa6a9bbad3aa923ed3ffd4be98493686b5fd8
IMPax478 <batchu.abhi@gmail.com>
2023-03-05 14:52:33 +0100

    Update Materials.py

--- Materials.py
+++ Materials.py
@@ -1,8 +1,8 @@
 import math as mth
 
-
+ 
 class Materials:
-    def __init__(self, material, density, yieldstress_l, yieldstress_u, Emod, OpTemp_l, OpTemp_u, cost,k):
+    def __init__(self, material, density, yieldstress_l, yieldstress_u, Emod, OpTemp_l, OpTemp_u, cost):
         self.material = material
         self.density = density
         self.yieldstress_l = yieldstress_l
@@ -11,35 +11,37 @@ class Materials:
         self.OpTemp_l = OpTemp_l
         self.OpTemp_u = OpTemp_u
         self.cost = cost
-        self.k=k
 
-Rhenium = Materials('Rhenium', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938,39600)
-Rhenium2 = Materials('Rhenium2', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938,39600)
+Rhenium = Materials('Rhenium', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938)
+Rhenium2 = Materials('Rhenium2', 21000, 1060*10**6, 2300*10**6,471*10**9, 0, 997, 938)
 
 def Material_Select(pressure):
     a = [Rhenium, Rhenium2]
     for i in a:
         if i.yieldstress_l > pressure:
             a = i.material
+            print(a)
     return a
 
 
-#Computing Mass: 
-x = [(1,2),(3,4),(5,6)]
- 
+##Computing Mass nozzle: 
+#Form Coordinates (integration of nozzle & material)
+def coordinate(a,b):
+    if len(a) != len(b):
+        raise Exception("#x-coordinates not equal to #y-coordiates for nozzle") 
+    coordinate = []
+    for i in range(len(a)):
+        coordinate.append((a[i],b[i]))
+    return x
+
+#Mass estimation function: 
 def Mass(x,t,material):
     total_dist = 0
     for i in range(len(x)-1):
         total_dist += mth.dist(x[i+1],x[i])
-    
     vol = total_dist*t*2*mth.pi
     Mass = vol*material.density
     return Mass
-
-
-Mass(x,0.1,Rhenium)
-Material_Select(25)
-
 
 
 
