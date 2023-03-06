288aedc3ddcd86310b2bccdce6bdaea40534782f
casruks <81488457+casruks@users.noreply.github.com>
2023-03-07 00:35:23 +0100

    Update Injector.py

--- Injector.py
+++ Injector.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
-    d_f = 0.000762                          # [LRE Injectors NASA, 1976] p.21, <0.03 in does not provide more atomization
-    #d_f = (0.0135 +0.281)*0.0254/2         # 3.74 mm # http://libgen.rs/book/index.php?md5=3D236B9BDD4070690CA83952058D9A1F p.113
+    #d_f = 0.000762                          # [LRE Injectors NASA, 1976] p.21, <0.03 in does not provide more atomization
+    d_f = (0.0135 +0.281)*0.0254/2         # 3.74 mm # http://libgen.rs/book/index.php?md5=3D236B9BDD4070690CA83952058D9A1F p.113
     #D_i = (0.02 +0.08)*0.0254/2            # 1.27 mm
     
     def Massflow(m, OF):
@@ -26,13 +26,19 @@ def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
     
     m_ox, m_f = Massflow(m, OF)
 
-    v_if = 21
-    n_f = 1
-    A_if = 0.25*np.pi*d_f**2    #Area for 1 orifice
+    v_if = v_iox = 21
+    n_f = n_ox = 1 
+    A_if = A_iox = 0.25*np.pi*d_f**2    #Area for 1 orifice
     while v_if > 20:
         n_f += 1
+        
         v_if = m_f / (rho_f*n_f*A_if)
-
+        
+    while v_iox > 20:
+        n_ox += 1
+        v_iox = m_ox / (rho_ox*n_ox*A_iox)
+    print('v_if =',v_if)
+    print('n_f =', n_f)
     p_center = p_j = 1
     if InjType == 'unlike': #liquid - liquid   
         mu_wax = 2.69e-3    # [lbm/(ft-s)], 1 lbm/(ft-s) = 1.4881639 Pa.s
@@ -47,19 +53,24 @@ def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
         b_triplet = 4/7
         b_pentad = 4/5
         b = [b_doublet, b_triplet, b_quadletA, b_quadletB, b_pentad]
-        R = k[0]*((rho_ox/rho_f)*(m_ox/m_f)**2)**b[0]   # orifice area ratio A_iox/A_if
-        A_ox = R * A_if*n_f                             # Total oxidizer orifice area 
-        v_iox = m_ox / (rho_ox*A_ox)                    # injection velocity per oxidizer orifice
-        n_ox = A_ox / A_if                              # req no. of ox orifices assuming A_iox = A_if
-        d_o = 2*(A_if / np.pi)**0.5
+        ## Leads to R = 12 due to 1141 and 71 LOX LH2, equation is based on LOX and RP-1, R ~1
+        # R = k[0]*((rho_ox/rho_f)*(m_ox/m_f)**2)**b[0]   # orifice area ratio A_iox/A_if
+        # print('R =', R)
+        # A_ox = R * A_if*n_f                             # Total oxidizer orifice area 
+        # print('A_ox =',A_ox)
+        # print('A_if*n_f =',A_if*n_f)
+        # v_iox = m_ox / (rho_ox*A_ox)                    # injection velocity per oxidizer orifice
+        # n_ox = A_ox / A_if                              # req no. of ox orifices assuming A_iox = A_if
+        # print('n_ox=', n_ox)
+        d_o = 2*(A_iox / np.pi)**0.5
                 
         # unlike doublet https://ntrs.nasa.gov/citations/19720010642
         # tested for d_f <= d_o 
         P_D = (rho_f*v_if**2)/(rho_ox*v_iox**2)
         #droplet size
-        D_f = (2.91e4*(v_if*3.28084)**(-0.76)*(d_f*39.3701)**(0.29)*(p_center/p_j)**(-0.65)*P_D**(0.165)*(d_o/d_f)**(0.023)*K_prop)*1e-6
-        D_o = (2.72e4*(v_iox*3.28084)**(-0.57)*(d_o*39.3701)**(0.65)*(p_center/p_j)**(-0.3)*P_D**(-0.25)*(d_o/d_f)**(-0.17)*K_prop)*1e-6
-        
+        D_f = (2.91e4*(v_if*3.28084)**(-0.76)*(d_f*39.3701)**(0.29)*(p_center/p_j)**(-0.65)*P_D**(0.165)*(d_o/d_f)**(0.023)*K_prop)*0.0254e-6
+        D_o = (2.72e4*(v_iox*3.28084)**(-0.57)*(d_o*39.3701)**(0.65)*(p_center/p_j)**(-0.3)*P_D**(-0.25)*(d_o/d_f)**(-0.17)*K_prop)*0.0254e-6
+        print(D_f,D_o, P_D)
     elif InjType == 'like':
         A_iox = A_if
         n_ox = n_f
@@ -74,8 +85,8 @@ def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
         # https://ntrs.nasa.gov/citations/19760023196
         d_j = d_f
         v_j = (v_iox + v_if)/2
-        D_f = D_o = (1.6e5*(v_j*3.28084)**(-1)*(p_center/p_j)**(-0.1)*(d_j*39.3701)**0.57*K_prop)*1e-6
-
+        D_f = D_o = (1.6e5*(v_j*3.28084)**(-1)*(p_center/p_j)**(-0.1)*(d_j*39.3701)**0.57*K_prop)*0.0254e-6
+        print(D_f)
     return v_iox, v_if, D_f, D_o
 
 def injector2(v_iox, v_if, D_f, D_o, p_inj, C_d, rho_ox, rho_f):
@@ -105,4 +116,13 @@ def injector2(v_iox, v_if, D_f, D_o, p_inj, C_d, rho_ox, rho_f):
     return p_c, dp_ox, dp_f
 
 def validateInj():
-    injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop)
+    C_d = 0.7
+    m = 12
+    OF = 1.7
+    rho_ox = 1141
+    rho_f = 71
+    mu_prop = 2.69e-3    
+    sig_prop = 17            
+    rho_prop = 47.7
+    print(injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop))
+#validateInj()
