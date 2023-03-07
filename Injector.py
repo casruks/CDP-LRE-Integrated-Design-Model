9485116707c2bcb7754d906223a70cb170157946
casruks <81488457+casruks@users.noreply.github.com>
2023-03-07 09:58:43 +0100

    Update Injector.py

--- Injector.py
+++ Injector.py
@@ -25,20 +25,23 @@ def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
     InjType = InjTypes[t]
     
     m_ox, m_f = Massflow(m, OF)
-
-    v_if = v_iox = 21
+    
+    # placeholder for reasonable injection velocities
+    v_if = v_iox = 351
     n_f = n_ox = 1 
     A_if = A_iox = 0.25*np.pi*d_f**2    #Area for 1 orifice
-    while v_if > 20:
+    while v_if > 320:
         n_f += 1
-        
         v_if = m_f / (rho_f*n_f*A_if)
         
-    while v_iox > 20:
+    while v_iox > 60:
         n_ox += 1
         v_iox = m_ox / (rho_ox*n_ox*A_iox)
-    print('v_if =',v_if)
-    print('n_f =', n_f)
+    # print('v_if =',v_if)
+    # print('n_f =', n_f)
+    # print('n_ox =', n_ox)
+    # print(m_ox/n_ox, m_f/n_f)
+    # print('A_f =',A_if*n_f, 'A_ox =', A_iox*n_ox, 'A_f/A_ox =', n_f/n_ox)
     p_center = p_j = 1
     if InjType == 'unlike': #liquid - liquid   
         mu_wax = 2.69e-3    # [lbm/(ft-s)], 1 lbm/(ft-s) = 1.4881639 Pa.s
@@ -70,7 +73,7 @@ def injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop):
         #droplet size
         D_f = (2.91e4*(v_if*3.28084)**(-0.76)*(d_f*39.3701)**(0.29)*(p_center/p_j)**(-0.65)*P_D**(0.165)*(d_o/d_f)**(0.023)*K_prop)*0.0254e-6
         D_o = (2.72e4*(v_iox*3.28084)**(-0.57)*(d_o*39.3701)**(0.65)*(p_center/p_j)**(-0.3)*P_D**(-0.25)*(d_o/d_f)**(-0.17)*K_prop)*0.0254e-6
-        print(D_f,D_o, P_D)
+        # print(D_f,D_o, P_D)
     elif InjType == 'like':
         A_iox = A_if
         n_ox = n_f
@@ -116,13 +119,16 @@ def injector2(v_iox, v_if, D_f, D_o, p_inj, C_d, rho_ox, rho_f):
     return p_c, dp_ox, dp_f
 
 def validateInj():
-    C_d = 0.7
-    m = 12
-    OF = 1.7
+    C_d = 0.8
+    m = 250
+    OF = 6
     rho_ox = 1141
     rho_f = 71
     mu_prop = 2.69e-3    
     sig_prop = 17            
     rho_prop = 47.7
+    v_iox, v_if, D_f, D_o = injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop)
+    p_inj = 182.1e5
     print(injector1(C_d, m, OF, rho_ox, rho_f, mu_prop, sig_prop, rho_prop))
-#validateInj()
+    print(injector2(v_iox, v_if, D_f, D_o, p_inj, C_d, rho_ox, rho_f))
+# validateInjx()
