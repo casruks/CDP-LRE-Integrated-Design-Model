e24acc29e0bc803341b23eac8ba685c64e76dbf0
Nick <mail@nickdejongc.com>
2023-04-01 11:12:54 +0200

    changed isp to specific vel

--- Nozzle_turbine.py
+++ Nozzle_turbine.py
@@ -6,8 +6,6 @@ import numpy as geek
 import rocketcea
 
 def Turbine_nozzle(m_p,Pc,Prop,Pamb,Default,h_fuel,h_ox,rho_fuel,rho_ox,MR):
-
-
     Ox=Prop.Ox_name
     Fuel=Prop.Fuel_name
     frozen_state=Prop.Frozen_state
@@ -16,8 +14,6 @@ def Turbine_nozzle(m_p,Pc,Prop,Pamb,Default,h_fuel,h_ox,rho_fuel,rho_ox,MR):
     Fuel_composition=Prop.Fuel_composition
     Pc = Pc/1.0e5
     Pamb = Pamb/1.0e5
-    
-
    
     card_str="""
     oxid {} {} wt%=100
@@ -29,19 +25,13 @@ def Turbine_nozzle(m_p,Pc,Prop,Pamb,Default,h_fuel,h_ox,rho_fuel,rho_ox,MR):
     fuel {} {} wt%=100
     h,kj/kg={} t(k)={} rho={}
     """.format(Fuel,Fuel_composition,str(h_fuel),'298',str(rho_fuel))
-
     add_new_fuel('Fuel_turbine',card_str)
-    print(card_str)
+
     ispObj = CEA_Obj( oxName=Ox, fuelName=Fuel,cstar_units='m/s',pressure_units='bar',temperature_units='K',isp_units='sec',enthalpy_units='kJ/kg',density_units='kg/m^3',specific_heat_units='J/kg-K',viscosity_units='poise')
 
-    h_f=rocketcea.blends.getFuelHfCalPerMole('Fuel_turbine')
-    h_f1=rocketcea.blends.getFuelHfCalPerMole('RP1')
     Ae_max=De_turbine_noz_max**2.0/4.0*mth.pi
     cs=ispObj.get_Cstar(Pc,MR)
     At=cs*m_p/(Pc*1.0e5)
-    variation=0.1
-    v_eff=-1.0
-
 
     eps_max=Ae_max/At # Maximum expansion ratio with this throat area
     Pratio_max=ispObj.get_PcOvPe(Pc=Pc,MR=MR,eps=eps_max,frozen=frozen_state,frozenAtThroat=frozen_state)
@@ -49,14 +39,12 @@ def Turbine_nozzle(m_p,Pc,Prop,Pamb,Default,h_fuel,h_ox,rho_fuel,rho_ox,MR):
 
     if Pe_max>Pamb:
         difference=0.001
-        Pe=Pe_max
         Ae=Ae_max; #In this case the maximum exit area cannot reach adapted conditions and we will simply take that
     else:
         difference=0.1
         Ae_1=At+0.000001; # In this case we enter the loop and start the iteration with Minimum exit area with this throat area
         
     while difference>0.01:
-
         eps_1= Ae_1/At # First expansion ratio 
         Pratio_1=ispObj.get_PcOvPe(Pc=Pc,MR=MR,eps=eps_1,frozen=frozen_state,frozenAtThroat=frozen_state)
         Pe_1=Pc/Pratio_1 # Exit pressure 1
@@ -64,17 +52,14 @@ def Turbine_nozzle(m_p,Pc,Prop,Pamb,Default,h_fuel,h_ox,rho_fuel,rho_ox,MR):
         difference=abs(Pe_1-Pamb)/Pamb 
 
         if difference>0.01:
-
             Ae_2=Ae_1*(Pe_1/Pamb)**0.3 # Change value for next iteration
             Ae_1=Ae_2;
         else:
             Ae=Ae_1 # Finish iteration if we have reached the convergence
-            Pe=Pe_1;
     eps_actual=Ae/At;
         
         
     Isp_it=ispObj.estimate_Ambient_Isp(Pc=Pc,MR=MR,eps=eps_actual,Pamb=Pamb,frozen=frozen_state,frozenAtThroat=frozen_state) # Calculates Isp for this iteration
     v_eff_it=Isp_it[0]*9.80665; # Calculates effective velocity for this iteration
     
-    Isp=Isp_it[0]
-    return Isp
+    return v_eff_it
